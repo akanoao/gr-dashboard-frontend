b98515cc9208168d1dd3ba47241fc8c83ea3a17e
plakshiyadav12 <sa6001533@gmail.com>
2024-11-25 12:05:20 +0530

    commit 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Define the Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['@emotion/react'],
+  },
+});
